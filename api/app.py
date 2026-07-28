923fe2aba80cf372517b2d749f99783a418ffa70
ayana-na <aya088660@gmail.com>
2026-07-28 23:11:39 +0300

    Refactor import statements in app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from .config import Config
-from services.ai_services import AIServices
+from .services.ai_services import AIServices
+from .recommender.recommendation_service import RecommendationService
+from .recommender.user_stage import UserStageDetector
 import pandas as pd
 from sqlalchemy import create_engine, text
 import logging
