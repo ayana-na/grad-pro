30f8e3494b0fc808f949f82286fb872707b3a81e
ayana-na <aya088660@gmail.com>
2026-07-29 18:50:59 +0300

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,9 +1,11 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "mysql+pymysql://root:@127.0.0.1/real_estate_ai")
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    MODEL_DIR = os.path.join(BASE_DIR, "models")
+    MODEL_DIR = os.path.join(BASE_DIR, "models") 
     CONVERSION_MODEL_PATH = os.path.join(MODEL_DIR, "conversion_model_optimized.pkl")
     CONVERSION_FEATURES_PATH = os.path.join(MODEL_DIR, "conversion_features_optimized.pkl")
     EMPLOYEE_MODEL_PATH = os.path.join(MODEL_DIR, "employee_matcher_xgb.pkl")
