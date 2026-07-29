d424b00155da16ea969f44e27f491df6a65fca94
ayana-na <aya088660@gmail.com>
2026-07-29 18:25:07 +0300

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,19 +1,14 @@
 import os
 
 class Config:
-       SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "mysql+pymysql://root:@127.0.0.1/real_estate_ai")
-    
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "mysql+pymysql://root:@127.0.0.1/real_estate_ai")
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     MODEL_DIR = os.path.join(BASE_DIR, "models")
-    
     CONVERSION_MODEL_PATH = os.path.join(MODEL_DIR, "conversion_model_optimized.pkl")
     CONVERSION_FEATURES_PATH = os.path.join(MODEL_DIR, "conversion_features_optimized.pkl")
     EMPLOYEE_MODEL_PATH = os.path.join(MODEL_DIR, "employee_matcher_xgb.pkl")
     EMPLOYEE_FEATURES_PATH = os.path.join(MODEL_DIR, "employee_matcher_features.pkl")
     EMPLOYEE_ENCODERS_PATH = os.path.join(MODEL_DIR, "employee_matcher_encoders.pkl")
-    
-    
     PORT = int(os.environ.get('PORT', 5001))
     HOST = '0.0.0.0'
-    DEBUG = False  
+    DEBUG = False
