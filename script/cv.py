d8e86a3ff7c189acb83e802c0de257f141d84804
ayana-na <aya088660@gmail.com>
2026-08-27 04:30:07 +0300

    Delete script/cv.py

--- script/cv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# استخدم شرطة مائلة للأمام أو raw string
-df = pd.read_csv("C:/xampp/htdocs/crm/script/AmesHousing.csv")
-
-print(df.info())
-print(df.describe())
-print(df.columns.tolist())
